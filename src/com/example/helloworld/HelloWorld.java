5d62cfc4379c2bbb908895aaf300127a8b0f2d70
Sıla <sila.saraoglu@gmail.com>
2021-02-19 13:26:51 +0300

    master:initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int count = 5;
+        System.out.println(count);
+    }
+
+    public static int multiply(int num1, int num2)
+    {
+        //TODO
+        return 0;
+    }
+}
